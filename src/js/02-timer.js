39ea7a4fe9ec5b49798717eae83a1d227fdf7437
AlexGoIT <alexandrpuryaev@gmail.com>
2023-04-11 18:13:50 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,8 +2,16 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
+
+
 const startBtn = document.querySelector('button[data-start]');
 startBtn.disabled = true;
+
+let interval;
 
 const options = {
   enableTime: true,
@@ -11,32 +19,44 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    startTimer(selectedDates[0]);
+    const today = new Date();
+    interval = selectedDates[0] - today;
+
+    if (interval < 0) {
+      interval = 0;
+      Notiflix.Notify.failure('Please choose a date in the future');
+      return;
+    }
+
+    viewTimer();
+    startBtn.disabled = false;
   },
 };
 
-const timer = flatpickr('#datetime-picker', options);
-
-function startTimer(selectedDates) {
-  const today = new Date();
-  let interval = selectedDates - today;
-
-  console.log(convertMs(interval));
-
-  if (interval < 0) {
-    Notiflix.Notify.failure('Please choose a date in the future');
-    return;
-  }
-
-  startBtn.disabled = false;
-}
+const fpr = flatpickr('#datetime-picker', options);
 
 startBtn.addEventListener('click', () => {
-  setTimeout(() => {
-    
-  })
-})
+  const timer = setInterval(() => {
+    interval -= 1000;
+
+    if (interval <= 0) {
+      clearInterval(timer);
+      Notiflix.Notify.success('Timer is over');
+      return;
+    }
+
+    viewTimer();
+  }, 1000);
+});
+
+function viewTimer() {
+  const timer = convertMs(interval);
+
+  days.textContent = timer.days < 10 ? `0${timer.days}` : timer.days;
+  hours.textContent = timer.hours < 10 ? `0${timer.hours}` : timer.hours;
+  minutes.textContent = timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes;
+  seconds.textContent = timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds;
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
