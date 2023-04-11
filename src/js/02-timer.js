9e8bd303cbed6f5341bdde186f4ea67ba7f30feb
AlexGoIT <alexandrpuryaev@gmail.com>
2023-04-11 16:00:51 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,9 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+const startBtn = document.querySelector('button[data-start]');
+startBtn.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -9,10 +12,31 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    startTimer(selectedDates[0]);
   },
 };
 
 const timer = flatpickr('#datetime-picker', options);
+
+function startTimer(selectedDates) {
+  const today = new Date();
+  let interval = selectedDates - today;
+
+  console.log(convertMs(interval));
+
+  if (interval < 0) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    return;
+  }
+
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    
+  })
+})
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
